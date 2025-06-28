fb79c8c0f33ea7e5b5ca7950045f2bd3c4814a21
Rohan Singh <singh.rohan7524@gmail.com>
2025-06-28 11:54:20 +0530

    added firebase connectivity and embedded google maps into the same.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFrqQQgFr42MFwA9Dcip4yxipjOAGxLRU",
+  authDomain: "bolthackathon-96e3b.firebaseapp.com",
+  projectId: "bolthackathon-96e3b",
+  storageBucket: "bolthackathon-96e3b.firebasestorage.app",
+  messagingSenderId: "304564134116",
+  appId: "1:304564134116:web:061addf51d01834e14ccf1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { MapPin, Clock, Users, Recycle, TrendingUp, Award } from 'lucide-react';
 import { StatsCard } from '../components/StatsCard';
